2357a33e916c77708909926f258328d9fd3c6fad
Dmitry Lobanov <gaussblurinc@gmail.com>
2026-09-04 11:33:24 +0300

    Apply requested changes.

--- macosx/FileOutlineView.mm
+++ macosx/FileOutlineView.mm
@@ -2,11 +2,8 @@
 // It may be used under the MIT (SPDX: MIT) license.
 // License text can be found in the licenses/ folder.
 
-#import "InfoWindowController.h"
-#import "FileListNode.h"
 #import "BaseFileNameCellView.h"
 #import "FileOutlineView.h"
-#import "Torrent.h"
 
 @interface FileOutlineView ()
 
