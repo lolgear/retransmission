161fe0bd5aab300d6bda3f10806b049cc6bb3d6d
Dmitry Lobanov <gaussblurinc@gmail.com>
2026-09-04 12:37:03 +0300

    Applying requested changes.

--- macosx/DragOverlayView.mm
+++ macosx/DragOverlayView.mm
@@ -11,7 +11,7 @@ static CGFloat const kBadgeWidth = 325.0;
 static CGFloat const kBadgeHeight = 84.0;
 
 // MainStack
-static CGFloat const kMainStackInset = 10;
+static CGFloat const kMainStackInset = 10.0;
 static CGFloat const kMainStackSpacing = 5.0;
 
 // Icon
@@ -22,7 +22,6 @@ static CGFloat const kTextStackSpacing = 2.0;
 
 @interface DragOverlayView ()
 
-@property(nonatomic, readonly) NSView* badgeContainer;
 @property(nonatomic, readonly) NSImageView* iconImageView;
 @property(nonatomic, readonly) NSTextField* titleLabel;
 @property(nonatomic, readonly) NSTextField* subtitleLabel;
@@ -34,41 +33,28 @@ static CGFloat const kTextStackSpacing = 2.0;
 - (instancetype)initWithFrame:(NSRect)frame
 {
     if ((self = [super initWithFrame:frame])) {
-        // 1. Background badge container
-        _badgeContainer = [[NSView alloc] initWithFrame:NSZeroRect];
-        _badgeContainer.translatesAutoresizingMaskIntoConstraints = NO;
-        _badgeContainer.wantsLayer = YES;
-        _badgeContainer.layer.backgroundColor = [NSColor colorWithCalibratedWhite:0.0 alpha:0.75].CGColor;
-        _badgeContainer.layer.cornerRadius = 15.0;
-        [self addSubview:_badgeContainer];
+        NSView* badgeContainer = [[NSView alloc] initWithFrame:NSZeroRect];
+        badgeContainer.translatesAutoresizingMaskIntoConstraints = NO;
+        badgeContainer.wantsLayer = YES;
+        badgeContainer.layer.backgroundColor = [NSColor colorWithCalibratedWhite:0.0 alpha:0.75].CGColor;
+        badgeContainer.layer.cornerRadius = 15.0;
+        [self addSubview:badgeContainer];
 
-        // 2. Icon
         _iconImageView = [[NSImageView alloc] initWithFrame:NSZeroRect];
         _iconImageView.imageScaling = NSImageScaleProportionallyUpOrDown;
 
-        // 3. Title
-        _titleLabel = [[NSTextField alloc] initWithFrame:NSZeroRect];
-        _titleLabel.editable = NO;
-        _titleLabel.selectable = NO;
-        _titleLabel.drawsBackground = NO;
-        _titleLabel.bordered = NO;
+        _titleLabel = [NSTextField labelWithString:@""];
         _titleLabel.font = [NSFont boldSystemFontOfSize:18.0];
-        _titleLabel.textColor = NSColor.whiteColor;
-        _titleLabel.maximumNumberOfLines = 1;
-        _titleLabel.lineBreakMode = NSLineBreakByTruncatingMiddle;
 
-        // 4. Subtitle
-        _subtitleLabel = [[NSTextField alloc] initWithFrame:NSZeroRect];
-        _subtitleLabel.editable = NO;
-        _subtitleLabel.selectable = NO;
-        _subtitleLabel.drawsBackground = NO;
-        _subtitleLabel.bordered = NO;
+        _subtitleLabel = [NSTextField labelWithString:@""];
         _subtitleLabel.font = [NSFont systemFontOfSize:14.0];
-        _subtitleLabel.textColor = NSColor.whiteColor;
-        _subtitleLabel.maximumNumberOfLines = 1;
-        _subtitleLabel.lineBreakMode = NSLineBreakByTruncatingMiddle;
 
-        // 5. Vertical TextStack
+        for (NSTextField* label in @[ _titleLabel, _subtitleLabel ]) {
+            label.textColor = NSColor.whiteColor;
+            label.maximumNumberOfLines = 1;
+            label.lineBreakMode = NSLineBreakByTruncatingMiddle;
+        }
+
         NSStackView* textStackView = [NSStackView stackViewWithViews:@[ _titleLabel, _subtitleLabel ]];
         textStackView.orientation = NSUserInterfaceLayoutOrientationVertical;
         textStackView.alignment = NSLayoutAttributeLeading;
@@ -77,7 +63,6 @@ static CGFloat const kTextStackSpacing = 2.0;
         // Prevent the vertical stack from stretching awkwardly in height
         [textStackView setContentHuggingPriority:NSLayoutPriorityDefaultHigh forOrientation:NSLayoutConstraintOrientationVertical];
 
-        // 6. Horizontal MainStack
         NSStackView* mainStackView = [NSStackView stackViewWithViews:@[ _iconImageView, textStackView ]];
         mainStackView.translatesAutoresizingMaskIntoConstraints = NO;
         mainStackView.orientation = NSUserInterfaceLayoutOrientationHorizontal;
@@ -87,27 +72,22 @@ static CGFloat const kTextStackSpacing = 2.0;
         // AppKit NSStackView edgeInsets push views INWARD, so we use positive numbers here
         mainStackView.edgeInsets = NSEdgeInsetsMake(0, kMainStackInset, 0, kMainStackInset);
 
-        [_badgeContainer addSubview:mainStackView];
+        [badgeContainer addSubview:mainStackView];
 
-        // 7. Core Auto Layout constraints
         [NSLayoutConstraint activateConstraints:@[
-            // Center the badgeContainer inside the DragOverlayView bounds
-            [_badgeContainer.centerXAnchor constraintEqualToAnchor:self.centerXAnchor],
-            [_badgeContainer.centerYAnchor constraintEqualToAnchor:self.centerYAnchor],
+            [badgeContainer.centerXAnchor constraintEqualToAnchor:self.centerXAnchor],
+            [badgeContainer.centerYAnchor constraintEqualToAnchor:self.centerYAnchor],
 
-            // Explicit dimensions for the badge background
-            [_badgeContainer.widthAnchor constraintEqualToConstant:kBadgeWidth],
-            [_badgeContainer.heightAnchor constraintEqualToConstant:kBadgeHeight],
+            [badgeContainer.widthAnchor constraintEqualToConstant:kBadgeWidth],
+            [badgeContainer.heightAnchor constraintEqualToConstant:kBadgeHeight],
 
-            // Explicit dimensions for the icon
             [_iconImageView.widthAnchor constraintEqualToConstant:kIconSize],
             [_iconImageView.heightAnchor constraintEqualToConstant:kIconSize],
 
-            // Stretch the main stack view to fill the badgeContainer entirely
-            [mainStackView.leadingAnchor constraintEqualToAnchor:_badgeContainer.leadingAnchor],
-            [mainStackView.trailingAnchor constraintEqualToAnchor:_badgeContainer.trailingAnchor],
-            [mainStackView.topAnchor constraintEqualToAnchor:_badgeContainer.topAnchor],
-            [mainStackView.bottomAnchor constraintEqualToAnchor:_badgeContainer.bottomAnchor]
+            [mainStackView.leadingAnchor constraintEqualToAnchor:badgeContainer.leadingAnchor],
+            [mainStackView.trailingAnchor constraintEqualToAnchor:badgeContainer.trailingAnchor],
+            [mainStackView.topAnchor constraintEqualToAnchor:badgeContainer.topAnchor],
+            [mainStackView.bottomAnchor constraintEqualToAnchor:badgeContainer.bottomAnchor]
         ]];
     }
     return self;
