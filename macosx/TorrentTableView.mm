3a09281ee257a7db8fb6066194e3df991262230f
Dmitry Lobanov <gaussblurinc@gmail.com>
2026-09-05 13:39:22 +0300

    Apply requested changes.

--- macosx/TorrentTableView.mm
+++ macosx/TorrentTableView.mm
@@ -587,6 +587,10 @@ static NSTimeInterval const kToggleProgressSeconds = 0.175;
     BOOL minimal = [self.fDefaults boolForKey:@"SmallView"];
     NSInteger row = [self rowForView:view];
 
+    if (row < 0) {
+        return;
+    }
+
     if (minimal) {
         if ([view isKindOfClass:[TorrentCellActionButton class]]) {
             SmallTorrentCell* smallCell = [self viewAtColumn:0 row:row makeIfNecessary:NO];
@@ -631,6 +635,10 @@ static NSTimeInterval const kToggleProgressSeconds = 0.175;
 {
     BOOL minimal = [self.fDefaults boolForKey:@"SmallView"];
     NSInteger row = [self rowForView:view];
+
+    if (row < 0) {
+        return;
+    }
 
     if (minimal) {
         if ([view isKindOfClass:[TorrentCellActionButton class]]) {
