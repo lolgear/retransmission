1d014d12cb67e4fd4f4ede260a06e595c221ec44
Dmitry Lobanov <gaussblurinc@gmail.com>
2026-09-06 13:48:50 +0300

    perf: optimize TorrentTableView updates and smooth out

--- macosx/Controller.mm
+++ macosx/Controller.mm
@@ -3446,7 +3446,7 @@ static auto getSettingsFromNSUserDefaults(NSUserDefaults* defaults)
 - (IBAction)toggleAvailabilityBar:(id)sender
 {
     [self.fDefaults setBool:![self.fDefaults boolForKey:@"DisplayProgressBarAvailable"] forKey:@"DisplayProgressBarAvailable"];
-    [self.fTableView display];
+    [self.fTableView needsDisplay];
 }
 
 - (IBAction)toggleStatusBar:(id)sender
